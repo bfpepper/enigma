20436351cd90dbd0af776aca42501c34c845a3d6
Ben Pepper <bfpepper87@gmail.com>
2016-07-10 12:34:02 -0600

    built most of encrypt.  still no tests

--- lib/encipher.rb
+++ lib/encipher.rb
@@ -5,94 +5,62 @@ require "pry"
 class Encipher
 
   def initialize
-    @offset = Offsets.new.generate_offset
-    @key = Key.new.generate_key
-    @supported_charecters = (' '..'z').to_a
-    @a_letters = []
-    @b_letters = []
-    @c_letters = []
-    @d_letters = []
+    @offset = Offsets.new.generate_offset #offset ||
+    @key = Key.new.generate_key #key ||
+    @supported_characters = (' '..'z').to_a
+    # @letters = [[],[],[],[]] same as how we do it below
+    @letters = {"0": [], "1": [], "2": [], "3": []}
+    @rotated_dictionary = []
+    @encrypted_letters = []
   end
 
   def riddle(input)
+    split_letters(input)
+    create_rotate_dictionary
+    swap_letters_with_rotated_dictionary
+    spit_out_encrypted_output
+  end
+
+  def split_letters(input)
     split_input = input.chars
     split_input.each_with_index do |letter, index|
-      if index == 0
-        @a_letters << letter
-      end
+      @letters[index % 4] << letter
+    end
+  end
 
-      if index == 1
-        @b_letters << letter
-      end
+  def spit_out_encrypted_output
+      @encrypted_letters[0].zip(*@encrypted_letters[1..-1]).flatten.compact.join("")
+  end
 
-      if index == 2
-        @c_letters << letter
-      end
-
-      if index == 3
-        @d_letters << letter
+  def swap_letters_with_rotated_dictionary
+    @letters.each do |key, value|
+      value.each do |letter|
+        @encrypted_letters[key] << @rotated_dictionary[key][letter] # "j", "k"
       end
     end
   end
 
-  def a_rotation
-    a_offset = @offset[0].to_i
-    a_key = @key[0..1].to_i
-    a_offset + a_key
+  def create_rotate_dictionary
+    @letters.each do |key, value|
+      rotation_amount = calculate_rotation(key)
+      rotated_support_chars = rotate_support_chars(rotation_amount)
+      @rotated_dictionary[key] = rotate_dictionary(rotated_support_chars)
+    end
   end
 
-  def b_rotation
-    b_offset = @offset[1].to_i
-    b_key = @key[1..2].to_i
-    b_offset + b_key
+  def calculate_rotation(position)
+    offset = @offset[position].to_i
+    key = @key[position..position+1].to_i
+    offset + key
   end
 
-  def c_rotation
-    c_offset = @offset[2].to_i
-    c_key = @key[2..3].to_i
-    c_offset + c_key
+  def rotate_support_chars(rotation_amount)
+    @supported_characters.rotate(rotation_amount).to_a
   end
 
-  def d_rotation
-    d_offset = @offset[3].to_i
-    d_key = @key[3..4].to_i
-    d_offset + d_key
+  def rotate_dictionary(rotated_support_chars)
+    @supported_characters.zip(rotated_support_chars).to_h
   end
 
-  def a_rotated_charecters
-    @supported_charecters.rotate(a_rotation).to_a
-  end
-
-  def b_rotated_charecters
-    @supported_charecters.rotate(b_rotation).to_a
-  end
-
-  def c_rotated_charecters
-    @supported_charecters.rotate(c_rotation).to_a
-  end
-
-  def d_rotated_charecters
-    @supported_charecters.rotate(d_rotation).to_a
-  end
-
-  def a_pair
-    a_paired_letters = @supported_charecters.zip(a_rotated_charecters)
-    hash[a_paired_letters]
-  end
-
-  def b_pair
-    b_paired_letters = @supported_charecters.zip(b_rotated_charecters)
-    hash[b_paired_letters]
-  end
-
-  def c_pair
-    c_paired_letters = @supported_charecters.zip(c_rotated_charecters)
-    hash[c_paired_letters]
-  end
-
-  def d_pair
-    d_paired_letters = @supported_charecters.zip(d_rotated_charecters)
-    hash[d_paired_letters]
-  end
 
 end
